cd9e058f5abfe299c0aba8b86df8b5d4b6a456ce
github username <ianjpollack@gmail.com>
2019-02-05 23:40:07 -0500

    img, title, and branding posted (needs formatting and styling)

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ const display5 = document.getElementById('box-5');
 const display6 = document.getElementById('box-6');
 
 const img1 = document.getElementById('img-1');
+const img2 = document.getElementById('img-2');
+const img3 = document.getElementById('img-3');
+const img4 = document.getElementById('img-4');
+const img5 = document.getElementById('img-5');
+const img6 = document.getElementById('img-6');
+
+const title1 = document.getElementById('title-1');
+const title2 = document.getElementById('title-2');
+const title3 = document.getElementById('title-3');
+const title4 = document.getElementById('title-4');
+const title5 = document.getElementById('title-5');
+const title6 = document.getElementById('title-6');
+
+const branding1 = document.getElementById('branding-1');
+const branding2 = document.getElementById('branding-2');
+const branding3 = document.getElementById('branding-3');
+const branding4 = document.getElementById('branding-4');
+const branding5 = document.getElementById('branding-5');
+const branding6 = document.getElementById('branding-6');
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -24,10 +43,52 @@ displayFetch = (data) => {
   console.log(dataArray);
   console.log("first", dataArray[0]);
   for (let i = 0; i < dataArray.length; i++) {
+    let targetImg = null
+    let targetTitle = null
+    let targetBranding = null
     let imgUrl = dataArray[i].thumbnail[0].url
-    console.log(imgUrl);
+    let imgH = dataArray[i].thumbnail[0].height
+    let imgW = dataArray[i].thumbnail[0].width
+    let title = dataArray[i].name
+    let branding = dataArray[i].branding
+    let link = dataArray[i].url
+    if (i === 0) {
+      targetImg = img1;
+      targetTitle = title1;
+      targetBranding = branding1;
+    } else if (i === 1) {
+      targetImg = img2;
+      targetTitle = title2;
+      targetBranding = branding2;
+    } else if (i === 2) {
+      targetImg = img3;
+      targetTitle = title3;
+      targetBranding = branding3;
+    } else if (i === 3) {
+      targetImg = img4;
+      targetTitle = title4;
+      targetBranding = branding4;
+    } else if (i === 4) {
+      targetImg = img5;
+      targetTitle = title5;
+      targetBranding = branding5;
+    } else if (i === 5) {
+      targetImg = img6;
+      targetTitle = title6;
+      targetBranding = branding6;
+    }
+    targetImg.src = imgUrl;
+    targetImg.height = imgH;
+    targetImg.width = imgW;
+    targetTitle.innerText = title;
+    targetBranding.innerText = branding;
+
+
+
+
   }
-  display1.innerHTML = "Yo!"
+  // display1.innerHTML = "Yo!"
+  console.log(img1);
 }
 
 
