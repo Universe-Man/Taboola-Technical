b88e1b03211db5e10a565b9a3af4302521bb9f1e
github username <ianjpollack@gmail.com>
2019-02-05 22:18:04 -0500

    getelementbyid working

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const API_URL = `https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init`
 
+const display1 = document.getElementById('box-1');
+const display2 = document.getElementById('box-2');
+const display3 = document.getElementById('box-3');
+const display4 = document.getElementById('box-4');
+const display5 = document.getElementById('box-5');
+const display6 = document.getElementById('box-6');
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM fully loaded and parsed");
+
+
+
+
     fetch(API_URL)
       .then(res => res.json())
       .then(json => displayFetch(json))
   });
 
 
+
+
 displayFetch = (data) => {
   console.log(data.list);
 }
+console.log(display1);
+console.log(document.getElementById('main-display'));
+
+
 
 
 
